648f42dba5d2d39b532d62c52f0003baf51ff722
imironjin <cjfwls9806@gmail.com>
2025-01-20 23:23:14 +0900

    chore: 라이브러리 설치 및 설정 추가 #6
    - sass, typescript-plugin-css-modules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import path from 'path';
+
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
 export default nextConfig;
