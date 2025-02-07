ecb09a81dfdf27953ae5c5849796ecdb13f9b606
Heesu Jang <heesu52@naver.com>
2025-02-07 20:18:56 +0900

    design: width를 고정 값이 아닌 %로 변경 #14

--- src/shared/components/button/positive-button.tsx
+++ src/shared/components/button/positive-button.tsx
@@ -1,15 +1,17 @@
 import styles from './positive-button.module.scss';
 
 interface PositiveButtonProps {
-  size?: 'large' | 'medium' | 'small'; // 버튼 크기 props 추가
+  size?: 'large' | 'medium' | 'small';
+  label: string;
 }
 
 export default function PositiveButton({
   size = 'large',
+  label,
 }: PositiveButtonProps) {
   return (
     <button className={`${styles.button} ${styles[size]}`}>
-      <span>로그인</span>
+      <span>{label}</span>
     </button>
   );
 }
