c1709ca42a8076cf7f3c975c570bf9f6072c8728
Heesu Jang <heesu52@naver.com>
2025-02-12 00:59:48 +0900

    design: 대리자 카드 퍼블리싱 추가 #18

--- src/app/concert/[id]/page.tsx
+++ src/app/concert/[id]/page.tsx
@@ -1,11 +1,63 @@
+import Link from 'next/link';
+
 import AppBarSetter from '@/shared/components/header/app-bar/app-bar-setter';
 
+import UserCard from './components/user-card/user-card';
 import styles from './page.module.scss';
 
 export default function Page() {
   return (
-    <div className={styles.container}>
+    <>
       <AppBarSetter title="콘서트 상세 페이지" />
-    </div>
+
+      <div className={styles.container}>
+        <div className={styles.title_container}>
+          <div className={styles.title}>
+            터치드(TOUCHED) 단독 콘서트 ‘HIGHLIGHT Ⅲ’
+          </div>
+
+          <div className={styles.detail_container}>
+            <div className={styles.detail}>
+              <span className={styles.category}>일자</span>
+              <span className={styles.info}>24/08/27 ~ 24/09/26</span>
+            </div>
+
+            <div className={styles.detail}>
+              <span className={styles.category}>장소</span>
+              <span className={styles.info}>올림픽공원 핸드볼 경기장</span>
+            </div>
+
+            <div className={styles.detail}>
+              <span className={styles.category}>예매처</span>
+              <span className={styles.info}>YES24</span>
+            </div>
+
+            <div className={styles.detail}>
+              <span className={styles.category}>무통장입금</span>
+              <span className={styles.info}>가능</span>
+            </div>
+
+            <Link className={styles.link} href="/">
+              정보 자세히 보기
+            </Link>
+          </div>
+        </div>
+
+        <hr className={styles.line} />
+        <div className={styles.list_container}>
+          <span className={styles.subtitle}>대리구매자 목록</span>
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
+      </div>
+    </>
   );
 }
