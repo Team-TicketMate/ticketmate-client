ad8afdff287a6ef4dbb3dff217ed55035a2d27ff
Heesu Jang <heesu52@naver.com>
2025-02-11 22:48:50 +0900

    design: header 삭제 후 app-bar로 변경 #18

--- src/app/concert/[id]/page.tsx
+++ src/app/concert/[id]/page.tsx
@@ -1,11 +1,11 @@
-import Header from '@/shared/components/header/header';
+import AppBarSetter from '@/shared/components/header/app-bar/app-bar-setter';
 
 import styles from './page.module.scss';
 
 export default function Page() {
   return (
     <div className={styles.container}>
-      <Header label="콘서트 상세 페이지" />
+      <AppBarSetter title="콘서트 상세 페이지" />
     </div>
   );
 }
